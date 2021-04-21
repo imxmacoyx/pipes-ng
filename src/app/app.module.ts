4be1e48fa3ab336154a59a9b886a98567dd8e7c4
MACOY <valle.hernandez.16199@itsmante.edu.mx>
2021-04-20 21:32:35 -0500

    Pipes angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+//Módulo personalizado de PrimeNg
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Cambiar el locale de la app
+import localeMx from '@angular/common/locales/es-MX';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeMx);
+registerLocaleData(localeFr);
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-MX' }] //Cambiar locale a Mx
+  ,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
